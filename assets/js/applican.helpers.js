32316e4c2955625824402848bb8dd6d286a8f958
Tuan Anh Nguyen <nguyenhuutuananh2@gmail.com>
2020-08-05 09:41:35 +0700

    #beacon - first implement

--- assets/js/applican.helpers.js
+++ assets/js/applican.helpers.js
@@ -38,3 +38,62 @@ if (typeof displayWebOnly === "undefined") {
         return content;
     }
 }
+var watchId;
+function beaconError(error){
+    var dump = "Error\n";
+    dump += "code:" + error.code + "\n";
+    alert(error);
+    alert(dump);
+}
+function initError(error) {
+    error.reason = 'init error';
+    beaconError(error);
+}
+function monitoringError(error) {
+    error.reason = 'monitoring error';
+    beaconError(error);
+}
+function watchError(error) {
+    error.reason += 'watch error';
+    beaconError(error);
+}
+function watchBeacon() {
+    console.log('watch beacon');
+    // 監視するビーコンの情報
+    var beaconInfo = { uuid: '2F8018F2-5725-4B06-B197-7BE925ACE9A3'};
+    // ビーコンの監視の開始
+    watchId = applican.beacon.watchBeacon(beaconInfo, watchBeaconResult, watchBeaconSuccess, watchError);
+    return watchId;
+}
+function watchBeaconSuccess(){
+    console.log('watch beacon success');
+    var dump = "watch id : " + watchId;
+    console.log(dump);
+}
+function watchBeaconResult(beacon) {
+    var uuid		= beacon.uuid;		// ビーコンの UUID
+    var major		= beacon.major;		// ビーコンのメジャー番号
+    var minor		= beacon.minor;		// ビーコンのマイナー番号
+    var proximity	= beacon.proximity;	// 距離の精度（0:不明, 1:至近距離, 2:近距離, 3:遠距離）
+    var accuracy	= beacon.accuracy;	// ビーコンまでの距離（メートル）
+    var rssi		= beacon.rssi;		// 受信信号強度（dBm）
+    var dump = "ビーコン情報:\n";
+    dump += "uuid : " + uuid + "\n";
+    dump += "major : " + major + "\n";
+    dump += "minor : " + minor + "\n";
+    dump += "proximity : " + proximity + "\n";
+    dump += "accuracy : " + accuracy + "\n";
+    dump += "rssi : " + rssi + "\n";
+    console.log(dump);
+}
+function startMonitoringSuccess(){
+    console.log('startMonitoring Success');
+    watchBeacon();
+}
+function initBeaconSuccess() {
+    console.log('initBeacon Success');
+    applican.beacon.startMonitoring(startMonitoringSuccess, monitoringError);
+}
+document.addEventListener('deviceready', function () {
+    applican.beacon.init(initBeaconSuccess, initError);
+});
