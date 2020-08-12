4dbb7437233b59cce7d8d8068471dd86d315bcd4
Tuan Anh Nguyen <nguyenhuutuananh2@gmail.com>
2020-08-12 11:02:37 +0700

    #beacon - add save beacon info for offline

--- assets/js/beacon_checkin.js
+++ assets/js/beacon_checkin.js
@@ -1,3 +1,8 @@
+const contractTermId = globalInfo("contract_term_id");
+const apiUrl = rootVariables.apiUrl;
+const jwt = globalInfo('jwt_' + contractTermId);
+const id = globalInfo('id_' + contractTermId);
+
 function EventProcessor() {
     const beaconInfoStatus = {
         PROCESSING: 0,
@@ -9,6 +14,7 @@ function EventProcessor() {
         HOLD_DATA: 3,
     };
     const minimumAccuracy = 1;
+    const throttleRequestTime = 60; // seconds
     const applicanWrapper = new ApplicanWrapper();
     this.getObjectData = function (key) {
         return applicanWrapper.simpleStorage.get(key).then((resp) => {
@@ -36,7 +42,7 @@ function EventProcessor() {
         //
         const oldBeaconInfo = await this.getObjectData(storeKey);
         if (oldBeaconInfo !== null) {
-            const newLastCall = new Date(oldBeaconInfo.last_call + 30 * 1000).getTime();
+            const newLastCall = new Date(oldBeaconInfo.last_call + throttleRequestTime * 1000).getTime();
             // if status is done or just call, nothing to do
             if (oldBeaconInfo.status === beaconInfoStatus.DONE || newLastCall > new Date().getTime()) return false;
             await this.saveObjectData(storeKey, {...beaconInfo, last_call: new Date().getTime(), status: beaconInfo.status || beaconInfoStatus.PROCESSING});
@@ -45,37 +51,49 @@ function EventProcessor() {
         await this.saveObjectData(storeKey, {...beaconInfo, last_call: new Date().getTime(), status: beaconInfoStatus.PROCESSING});
         return true;
     }
-    this.displayLocalNotification = function (fireDateUnixTime, message, alertId, url) {
-        var options = {
+    this.displayLocalNotification = function (fireDateUnixTime, repeat, message, alertId, url) {
+        const options = {
             alertBody: message,
             uri: url,
             fireDate: fireDateUnixTime,
-            repeatInterval: "minute",
             alertAction: "開く",				//iOSのみ
             applicationIconBadgeNumber: 1,			//iOSのみ
         };
         if (typeof alertId !== "undefined") {
             options.alertId = alertId;
         }
+        if (typeof repeat !== "undefined" && repeat !== false) {
+            options.repeatInterval = repeat; // "minute"
+        }
         return applicanWrapper.localNotification.schedule(options);
     }
-    this.saveBeaconInfoForRegister = function (beaconInfo) {
-
+    this.saveBeaconInfoForRegister = async function (beaconInfo) {
+        const saveKey = beaconInfo.uuid + beaconInfo.major;
+        let offlineInfoMap = await this.getObjectData('offlineInfoMap') || [];
+        let offlineInfo = await this.getObjectData('offlineInfo') || [];
+        // if exists do not save again
+        if (offlineInfoMap.findIndex(saveKey) === -1) return;
+        offlineInfo.push(beaconInfo);
+        offlineInfoMap.push(saveKey);
+        await this.saveObjectData('offlineInfoMap', offlineInfoMap);
+        await this.saveObjectData('offlineInfo', {...offlineInfo, save_date: new Date().getTime()});
     };
     this.preRegister = async function (beaconInfo) {
         if (!await this.filterBeaconInfo(beaconInfo)) return;
         // display when user didn't login
         if (!isUserLoggedIn()) {
-            this.displayLocalNotification(new Date().getTime() / 1000 + 5, 'ログイン後にイベント受付が可能です', NotificationType.NOT_LOGGED_IN);
+            this.displayLocalNotification(new Date().getTime() / 1000 + 5, false, 'ログイン後にイベント受付が可能です', NotificationType.NOT_LOGGED_IN);
+            await this.saveBeaconInfoForRegister(beaconInfo);
             return;
         }
         // display local notification when offline
-        // if (!isOnline()) {
-        //     this.displayLocalNotification(new Date().getTime() / 1000 + 5, 'ログイン後にイベント受付が可能です');
-        //     return;
-        // }
-        // console.log('register ', beaconInfo.uuid, beaconInfo.major);
-        // await this.register(beaconInfo);
+        if (!isOnline()) {
+            this.displayLocalNotification(new Date().getTime() / 1000 + 5, false,'参加を受け付けましたが、完了しませんでした。電波状況の良い場所で再度アプリを起動することで再度参加処理が行われます。');
+            await this.saveBeaconInfoForRegister({...beaconInfo, user_id: id});
+            return;
+        }
+        console.log('register ', beaconInfo.uuid, beaconInfo.major);
+        await this.register(beaconInfo);
     };
     this.register = async function (beaconInfo) {
         const storeKey = this.generateStoreKey(beaconInfo);
